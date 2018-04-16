f1c53b45b6b33d9d9119f089138230efc2376d11
zhouzh <363420113@qq.com>
2018-04-16 21:06:49 +0800

    提交demo代码

--- src/pages/template/advForm.vue
+++ src/pages/template/advForm.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>asdad</div>
+  <div class="layout-form">
+    <Form :model="formValidate" :label-width="80" style="width: 600px">
+      <FormItem label="用户名" prop="name">
+        <Input v-model="formValidate.name" placeholder="请输入用户名"/>
+      </FormItem>
+      <FormItem label="邮箱" prop="mail">
+        <Input v-model="formValidate.mail" placeholder="请输入邮箱"/>
+      </FormItem>
+    </Form>
+  </div>
 </template>
 
 <script>
     export default {
-
+      data() {
+        return {
+          formValidate: {
+            name: '',
+            mail: ''
+          }
+        }
+      }
     }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import test from './modules/test'
 import app from './modules/app'
+import project from './modules/project'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     test,
-    app
+    app,
+    project
   }
 })
